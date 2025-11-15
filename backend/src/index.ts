4ff2cc3ab553ee33c92c105ca325cce393d2909b
HCDEVE <houcineessayadfr99@gmail.com>
2025-11-15 12:34:48 +0100

    update backend config for prod render deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,5 @@
-// import express from "express";
-
-// const app = express();
-
-// app.use(express.json());
-
-// // Routes
-// app.use("/api/", itemRoutes);
-
-// // Global error handler (should be after routes)
-// app.use(errorHandler);
-
-// export default app;
-
-import { createServer } from "./server";
 import config from "./config/config";
+import { createServer } from "./server";
 
 const app = createServer();
 
